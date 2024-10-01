2f4b6459e202501f7a97bf81acced64a28351bf4
jaoBJ <jpmalhaofelix11@gmail.com>
2024-10-01 10:42:03 -0300

    ajuste do media query da home e do rodape

--- src/Components/Pages/Home/SectionC/index.jsx
+++ src/Components/Pages/Home/SectionC/index.jsx
@@ -7,14 +7,14 @@ import React from 'react'
 const SectionC = () => {
   return (
     <section className='section-c-home'>
-        <div className='section-c-home-img'>
-            <img src={img001} alt="imagem orçamento" />
-        </div>
-        <div className='section-c-home-textos'>   
-            <h2 className='section-c-home-textos-titulo'>Solicitação de Orçamento</h2>
-            <p className='section-c-home-textos-paragrafo'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere numquam odio quae, impedit accusantium alias facilis quam dolorem tempora officia expedita perferendis perspiciatis unde? Autem unde quisquam eaque iusto deserunt? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos culpa consequuntur sed id animi facilis dolores voluptatum similique doloribus repellendus numquam, eius cum exercitationem magnam eaque alias eveniet, recusandae eum?</p>
-            <Botao texto='Fale conosco'/>
-        </div>
+      <div className='section-c-home-img'>
+        <img src={img001} alt="imagem orçamento" />
+      </div>
+      <div className='section-c-home-textos'>
+        <h2 className='section-c-home-textos-titulo'>Solicitação de Orçamento</h2>
+        <p className='section-c-home-textos-paragrafo'>Estamos construindo a nova infraestrutura tecnológica para médias e grandes empresas no Brasil. Através de consultoria, diagnóstico e auditoria ajudamos empresas a serem mais eficientes e organizadas. Com nosso suporte técnico em informatica e TI, remoto ou presencial, reduzimos os custos em suas operações tecnológicas.</p>
+        <Botao texto='Fale conosco' />
+      </div>
     </section>
   )
 }
